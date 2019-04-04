4dbb80afd191a95ea0b758a24cc52a8bcbdce1c6
Aniudh Rana <anirudh80018@gmail.com>
2019-04-04 19:01:02 +0530

    fourth commit

---
+++ d3.py
@@ -0,0 +1,8 @@
+x=int(input())
+if(x%3 == 1):
+	print("Remainder is 1")
+elif(x%3 == 2):
+	print("Remainder is 2")
+else:
+	print("It is divisible by 3")
+
---
+++ whileloop1.py
@@ -0,0 +1,5 @@
+x= int(input("How many times do you want the loop to run\n"))
+while(x<=11):
+	print (x)
+	x+=1
+
