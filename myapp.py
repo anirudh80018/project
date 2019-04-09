570a6bbe6e0aa1199a3216de309ba6f345e8af1c
Aniudh Rana <anirudh80018@gmail.com>
2019-04-09 18:39:05 +0530

    flask form added

--- myapp.py
+++ myapp.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app= Flask(__name__)
 @app.route('/')
 def hello():
-	return "Hello I am Running on localhost and 5000 point"
+	return render_template("form.html")
+@app.route('/fb')
+def fb():
+	return "Welcome to facebook"
+@app.route('/form', methods=["post"])
+def form():
+	name= request.form["name"]
+	enroll= request.form["eno"]
+	email= request.form["email"]
+	return render_template("normal.html",name=name,eno=enroll,email=email)
 if __name__ == '__main__':
-	app.run(port=8000)
+	app.run(port=5000, debug=True)
+
